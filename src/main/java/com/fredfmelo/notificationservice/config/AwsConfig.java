dbfdf1e64183284c5fdefc20dfa731ac4b2a958f
Fred Melo <fredfmelo@gmail.com>
2026-06-01 09:15:25 -0300

    removing fixed aws-region and adding actuator for health checks

--- src/main/java/com/fredfmelo/notificationservice/config/AwsConfig.java
+++ src/main/java/com/fredfmelo/notificationservice/config/AwsConfig.java
@@ -4,41 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class AwsConfig {
 
-    private static final Region REGION = Region.US_EAST_1;
-
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(REGION)
                 .build();
     }
 
     @Bean
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient client) {
         return DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
+                .dynamoDbClient(client)
                 .build();
     }
 
     @Bean
     public SnsClient snsClient() {
         return SnsClient.builder()
-                .region(REGION)
-                .build();
-    }
-
-    @Bean
-    public SqsClient sqsClient() {
-        return SqsClient.builder()
-                .region(REGION)
                 .build();
     }
 }
